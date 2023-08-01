de81bd3e509e6cd4756c0f7707467a6e837cde56
nubizya <nunya.bizns@icloud.com>
2023-08-01 18:52:00 +0530

    config

--- src/app/config.py
+++ src/app/config.py
@@ -1,6 +1,8 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    app_name: str = "Vil"
-    admin_email: str = "admin@vil.com"
+    app_name: str
+    admin_email: str
+
+    model_config = SettingsConfigDict(env_file="env.local")
